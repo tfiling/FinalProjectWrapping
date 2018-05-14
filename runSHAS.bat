7ff2028336b8edb7b45afb1712516f5273644fee
tfiling <tfiling@auth.ad.bgu.ac.il>
2018-05-14 16:28:26 +0300

    cmpleted .bat runner for windows

--- runSHAS.bat
+++ runSHAS.bat
@@ -1,2 +1,28 @@
+@echo off
 CD SHAS
-start java -jar ..\start.jar
+start "SHAS" java -jar ..\start.jar
+timeout 5
+start http://localhost:8888/FinalProject-1.0-SNAPSHOT/
+:loop
+echo.
+echo *************************************************************
+echo ****                   Welcome to SHAS                   ****
+echo *************************************************************
+echo.
+echo Please Insert a command
+echo kill - will terminate the simulator
+echo open - will open the default web browser with the SHAS local url
+echo. 
+set /p command=""
+echo.
+if %command% == kill goto killApp
+if %command% == open goto openBrowser
+goto loop
+
+
+:openBrowser
+start http://localhost:8888/FinalProject-1.0-SNAPSHOT/
+goto loop
+
+:killApp
+taskkill /F /FI "WindowTitle eq SHAS" /T
